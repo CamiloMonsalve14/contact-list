95d54a171294388f2b3d749fa828ef22cb330db3
CamiloMonsalve14 <monsalvemaderac@gmail.com>
2023-08-22 22:23:00 -0500

    lista de contactos

---
+++ listaContactos.js
@@ -0,0 +1,22 @@
+let listaContactos = ["FelipeCalvache","CarlosPerez", "AbnerJimenez"];
+
+function agregarContacto (nuevoContacto){
+    listaContactos.push(nuevoContacto)
+}
+
+function borrarContacto (borrarContacto){
+let nuevalista = listaContactos.filter((contacto) => contacto !== borrarContacto);
+listaContactos = nuevalista
+}
+
+borrarContacto("JhonFlorez");
+console.log(listaContactos)
+
+function imprimirContacto () {
+    console.log(listaContactos);
+}
+agregarContacto("CamiloMonsalve")
+imprimirContacto();
+borrarContacto("CarlosPerez")
+imprimirContacto();
+
